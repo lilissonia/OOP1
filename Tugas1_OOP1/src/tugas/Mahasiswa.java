81103353500f113b6db8c9af8cc440adafc2c569
lilissonia <55830224+lilissonia@users.noreply.github.com>
2020-03-20 10:31:56 +0700

    Add files via upload

---
+++ Tugas1_OOP1/src/tugas/Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author lilissonia
+ */
+public class Mahasiswa {
+    
+    public static void main(String[] args) {
+      String nama = "Dimas Ilham M";
+      String kelas = "4C";
+      String nim = "18090122";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+    
+}
